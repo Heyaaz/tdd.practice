c960484c344b7511808d44d371de0a1653e0d09c
byeonheejae <pion0458@gmail.com>
2025-04-04 11:42:28 +0900

    멤버십이 존재하는지 테스트

--- src/main/java/com/team/project/tdd/MembershipRepository.java
+++ src/main/java/com/team/project/tdd/MembershipRepository.java
@@ -4,4 +4,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface MembershipRepository extends JpaRepository<MemberShip, Long> {
 
+  MemberShip findByUserIdAndMembershipType(String userId, MembershipType membershipType);
 }
--- src/test/java/com/team/project/tdd/MembershipRepositoryTest.java
+++ src/test/java/com/team/project/tdd/MembershipRepositoryTest.java
@@ -30,4 +30,26 @@ public class MembershipRepositoryTest {
     assertThat(result.getPoint()).isEqualTo(10000);
 
   }
+
+  @Test
+  void 멤버십이존재하는지테스트() {
+    // Given
+    final MemberShip memberShip = MemberShip.builder()
+        .userId("userId")
+        .membershipType(MembershipType.NAVER)
+        .point(10000)
+        .build();
+
+    // When
+    membershipRepository.save(memberShip);
+    final MemberShip findResult = membershipRepository.findByUserIdAndMembershipType ("userId", MembershipType.NAVER);
+
+    // Then
+    assertThat(findResult).isNotNull();
+    assertThat(findResult.getId()).isNotNull();
+    assertThat(findResult.getUserId()).isEqualTo("userId");
+    assertThat(findResult.getMembershipType()).isEqualTo(MembershipType.NAVER);
+    assertThat(findResult.getPoint()).isEqualTo(10000);
+
+  }
 }
